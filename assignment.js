d0a5f4ec316fec32686b0bebd8f84a8c1e6b5ca7
Roy-Susmita <susmita.mucse@gmail.com>
2021-01-22 19:46:23 +0600

    js code added

---
+++ assignment.js
@@ -0,0 +1,77 @@
+// Problem 1-kilometerToMeter
+function kilometerToMeter(kiloMeter) {
+    if (kiloMeter < 0) {
+        return "KiloMeter can not be negative";
+    }
+    else {
+        var Meter = 1000 * kiloMeter;
+        return Meter;
+    }
+}
+
+//var convert = kilometerToMeter(-5);
+//var convert = kilometerToMeter(25);
+//console.log(convert);
+
+// Problem 2-budgetCalculator
+function budgetCalculator(watch, mobile, laptop) {
+    if (watch < 0 || mobile < 0 || laptop < 0) {
+        return "Negative value is not acceptable";
+    }
+    else {
+        var totalBudget = watch * 50 + mobile * 100 + laptop * 500;
+        return totalBudget;
+    }
+}
+
+//var total = budgetCalculator(5, 2, -1);
+//console.log(total);
+
+// Problem 3-hotelCost
+function hotelCost(stayingDays) {
+    var Cost = 0;
+    if (stayingDays < 0) {
+        return "Day can not be negative";
+    }
+    else if (stayingDays <= 10) {
+        var Cost = stayingDays * 100;
+    }
+    else if (stayingDays <= 20) {
+        var firstTenDays = 10 * 100;
+        var daysRemain = stayingDays - 10;
+        var secondTenDays = daysRemain * 80;
+        Cost = firstTenDays + secondTenDays;
+    }
+    else {
+        var firstTenDays = 10 * 100;
+        var secondTenDays = 10 * 80;
+        var daysRemain = stayingDays - 20;
+        var restDays = daysRemain * 50;
+        Cost = firstTenDays + secondTenDays + restDays;
+
+    }
+    return Cost;
+}
+
+//var totalcost = hotelCost(41);
+//console.log(totalcost);
+
+// Problem 4-megaFriend
+function megaFriend(friendsName) {
+    var nameLength = 0;
+    if (friendsName == 0) {
+        return "There is no name";
+    }
+    else {
+        for (var i = 0; i < friendsName.length; i++) {
+            if (friendsName[i].length > nameLength) {
+                nameLength = friendsName[i].length;
+                var longestName = friendsName[i];
+            }
+        }
+    }
+    return longestName;
+
+}
+//var Name = megaFriend(["Susmi", "Susmita", "Lee min ho", "park shin hye"]);
+//console.log(Name);
